ff93d36a1a1ae196d6e0da4820b773cbca2f53f4
Patrick Brochu <patrickbrochu70@gmail.com>
2025-02-28 17:23:21 -0500

    Completed the contact section in javascript
    TODO:Fixing the issue with span element not
    appending to the p element

--- src/functionScript.js
+++ src/functionScript.js
@@ -11,8 +11,43 @@ for(let i = 0;i<3;i++){//generate 3 buttons
 appending(contentSelector, tabsWrapper);
 let tabsHome = document.createElement("div")
 tabsHome.classList.add("tabs-display-home")
-appending(contentSelector, tabsHome)
+appending(contentSelector, tabsHome);
+let tabsMenu = document.createElement("div");
+tabsMenu.classList.add("tabs-display-menu");
+appending(contentSelector, tabsMenu);
 
+//Creation of the contact page
+let tabsContact = document.createElement("div");
+tabsContact.classList.add("tabs-display-contact");
+let contactTitle = document.createElement("h1");
+appending(tabsContact, contactTitle);
+contactTitle.textContent = "Contact informations";
+createHr(tabsContact);
+
+let contactMail = document.createElement("p");
+//addSpan(contactMail, "Email:");        <-------------------              
+contactMail.textContent = "Raymond@gmail.com";
+appending(tabsContact, contactMail);
+createHr(tabsContact);
+
+let contactPhone = document.createElement("p");
+//addSpan(contactPhone, "Phone:");        <-------------------
+contactPhone.textContent = "514-665-8989";
+appending(tabsContact, contactPhone);
+createHr(tabsContact);
+
+let contactAddress = document.createElement("p");
+//addSpan(contactAddress, "Address:");        <-------------------
+contactAddress.textContent = "Entre 2 villages creux drette sul bord dla 132";
+appending(tabsContact, contactAddress);
+createHr(tabsContact);
+
+let contactWebsite = document.createElement("p");
+//addSpan(contactWebsite, "Website:");        <-------------------
+contactWebsite.textContent = "www.snackbarchezraymond.com";
+appending(tabsContact, contactWebsite);
+//appending the whole contact tab to the content wrapper
+appending(contentSelector, tabsContact);
 
 
 function sayHello(){
@@ -23,5 +58,15 @@ function appending(parent, child){//automated append function
     parent.append(child);
 }
 
+function addSpan(pElement, text){//a corriger
+    let spanElement = document.createElement("span");
+    spanElement.textContent = "ALLO";
+    pElement.prepend(spanElement);
+    console.log(`function span started with parent: ${pElement} the span created:${spanElement} with this text:${text}`);
+}
 
+function createHr(parent){
+    let separationLine = document.createElement("hr");
+    parent.append(separationLine);
+}
 export {sayHello};
